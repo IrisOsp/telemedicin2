dbc9e26577954a77fb5606723c6978d0bdc721cd
IrisOsp <irisjohannesdottir@gmail.com>
2024-04-27 15:05:13 +0200

    database virker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,28 +15,28 @@
         });
         const text = await result.text();
         if (result.status===200) {
-            window.location='/home';
+            window.location='/menu';
         } else {
             alert("forkert cpr eller kodeord");
         }
     };
 </script>
 
-<div style="display: flex; justify-content: center; align-items: center; margin-top:30px;">
-    <img src="./src/routes/images/logo1.jpg" alt="HealingHub logo" style="max-width: 300px; max-height: 300px; border-radius: 5px;">
+<div style="display: flex; justify-content: center; align-items: center; margin-top:80px; margin-bottom:30px;">
+    <img src="./src/routes/images/logo1.jpg" alt="HealingHub logo" style="max-width: 500px; max-height: 500px; border-radius: 5px;">
 </div>
 
-<div style="display: flex; justify-content: center; align-items: center; margin-top:60px;">
-<h3>Login</h3>
-</div>
-<div style="display: flex; justify-content: center; align-items: center; margin-top:20px;">
-    <input bind:value={username} type="text" placeholder="Indtast CPR"/></div>
-<div style="display: flex; justify-content: center; align-items: center; margin-top:15px;">
-    <input bind:value={password} type="password" placeholder="Kodeord"/>
-</div>
-<div style="display: flex; justify-content: center; align-items: center; margin-top:30px;">
-    <button color="primary" on:click={login}>login</button>
-</div>
+<div class="container ">
+
+    <div class="input-group w-25 m-auto" style="display: flex; justify-content: center; align-items: center; margin-top:20px;">
+        <input class="form-control " bind:value={username} type="text" placeholder="Indtast CPR"/></div>
+        <div class="input-group w-25 m-auto mt-2" style="display: flex; justify-content: center; align-items: center; margin-top:20px;">
+            <input class="form-control" bind:value={password} type="password" placeholder="Kodeord"/>
+        </div>
+        <div style="display: flex; justify-content: center; align-items: center;margin-top:10px;">
+            <button class="btn custom-navy-btn btn-sm mt-2" on:click={login}>Login</button>
+        </div>
+    </div>
 
 
 <style>
@@ -48,6 +48,16 @@
         justify-content: center;
         align-items: center;
         margin-top: 20px;
+    }
+    .custom-navy-btn {
+        background-color: navy;
+        color: white;
+        border-color: navy;
+    }
+    
+    .custom-navy-btn:hover {
+        background-color: darkblue;
+        border-color: darkblue;
     }
 
 </style>
