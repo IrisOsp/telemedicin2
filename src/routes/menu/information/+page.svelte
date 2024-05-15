45057a231b1a0bb287d3ef385a78e8896831b3d8
IrisOsp <irisjohannesdottir@gmail.com>
2024-05-15 14:25:38 +0200

    mmmmmmm

--- src/routes/menu/information/+page.svelte
+++ src/routes/menu/information/+page.svelte
@@ -1,28 +1,7 @@
 <script>
-    import { Button } from '@sveltestrap/sveltestrap';
 
-const logout = async () => {
-const result = await fetch('/api/logout', {
-    method: 'GET',
-    headers: {
-        "Content-type": "application/json"
-    },
-
-});
-
-if (result.status === 200) {
-    window.location = '/';
-} else {
-    alert("Der opstod en fejl under logud.");
-}
-};
 </script>
 
-<div style="position: absolute; top: 0; right: 0; margin-right: 10px;">
-    <div style="display: flex; justify-content: flex-end;">
-        <button class="btn custom-navy-btn btn-sm mt-2" on:click={logout}>Logout</button>
-    </div>
-</div>
 
 <div class="navigation">
 information
